5c2b210072717b93f7ce652c696029e509006ff8
青龍聖者@bdsqlsz <865105819@qq.com>
2023-12-26 16:45:35 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -8,11 +8,11 @@ if (!(Test-Path -Path "venv")) {
 }
 .\venv\Scripts\activate
 
-python -m pip install pip==23.0.1 -i https://mirror.baidu.com/pypi/simple
+python -m pip install pip==23.0.1
 
-pip install torch==2.1.1+cu121 torchvision==0.16.1+cu121 -f https://mirror.sjtu.edu.cn/pytorch-wheels/torch_stable.html -i https://mirror.baidu.com/pypi/simple
+pip install torch==2.1.1+cu121 torchvision==0.16.1+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
 
-pip install --no-deps xformers==0.0.23 -i https://mirror.baidu.com/pypi/simple
+pip install --no-deps xformers==0.0.23
 
 Write-Output "Installing deps..."
 
@@ -24,11 +24,11 @@ pip install --pre tensorrt --extra-index-url https://pypi.nvidia.com
 
 pip install pywin32 -i https://mirror.baidu.com/pypi/simple
 
-pip install -r examples/screen/requirements.txt -i https://mirror.baidu.com/pypi/simple
+pip install -r examples/screen/requirements.txt
 
-pip install -r demo/realtime-txt2img/requirements.txt -i https://mirror.baidu.com/pypi/simple
+pip install -r demo/realtime-txt2img/requirements.txt
 
-pip install accelerate -i https://mirror.baidu.com/pypi/simple
+pip install accelerate
 
 Write-Output "Installed Finish"
 Read-Host | Out-Null ;
